c4c8b6382fb857029bfae69aa00867dd535442db
Joel Rosenstam <rosenstam.joel@gmail.com>
2022-02-16 16:04:36 +0100

    Updated main

--- src/Main.java
+++ src/Main.java
@@ -4,5 +4,7 @@ public class Main {
         System.out.println("This is some text.");
         SuperflousPrinter superflousPrinter = new SuperflousPrinter();
         superflousPrinter.print();
+        Dice dice = new Dice();
+        System.out.println("Rolling a six-sided die, result is: " + dice.rollD6());
     }
 }
